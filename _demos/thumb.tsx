412e57f1359a802ddb4de20b37540e0d0181dee7
derek ho <sakuraderek@gmail.com>
2022-05-20 12:27:09 +0800

    feat: thumb add links & note

--- _demos/thumb.tsx
+++ _demos/thumb.tsx
@@ -3,19 +3,22 @@
  *
  */
 // @ts-ignore
-const Thumb = ({ src, alt, width, height }) => {
+const Thumb = ({ src, alt, width, height, note }) => {
   const aspectRatio = `${width} / ${height}`
   return (
-    <img
-      src={src}
-      alt={alt}
-      referrerPolicy="no-referrer"
-      loading="lazy"
-      style={{
-        width,
-        aspectRatio,
-      }}
-    />
+    <a className="thumb-anchor" href={src} target="_blank" rel="noreferrer">
+      <img
+        src={src}
+        alt={alt}
+        referrerPolicy="no-referrer"
+        loading="lazy"
+        style={{
+          width,
+          aspectRatio,
+        }}
+      />
+      {note ? <span className="note">{note}</span> : null}
+    </a>
   )
 }
 
