af42bc8837a956945779004b9e96a2fb86f5668c
Derek <sakuraderek@gmail.com>
2022-07-15 11:34:23 +0800

    chore: separate tests

--- __tests__/parse-queray-path.ts
+++ __tests__/parse-queray-path.ts
@@ -1,20 +1,32 @@
 import parseQueryPath from '../utils/parse-query-path'
 
-test('parse query path', () => {
-  expect(parseQueryPath([])).toMatchObject({})
-  expect(parseQueryPath(['tag', '1'])).toMatchObject({
-    tag: '1',
+describe('Parse query path', () => {
+  test('empty query', () => {
+    expect(parseQueryPath([])).toMatchObject({})
   })
-  expect(parseQueryPath(['page', '2'])).toMatchObject({
-    page: 2,
+  test('single param only tag', () => {
+    expect(parseQueryPath(['tag', '1'])).toMatchObject({
+      tag: '1',
+    })
   })
-  expect(parseQueryPath(['a', 'b', 'c', 'd'])).toMatchObject({})
-  expect(parseQueryPath(['tag', '2', 'page', '1'])).toMatchObject({
-    page: 1,
-    tag: '2',
+  test('single param only page', () => {
+    expect(parseQueryPath(['page', '2'])).toMatchObject({
+      page: 2,
+    })
   })
-  expect(parseQueryPath(['page', '1', 'tag', '2'])).toMatchObject({
-    page: 1,
-    tag: '2',
+  test('invalid params should be empty', () => {
+    expect(parseQueryPath(['a', 'b', 'c', 'd'])).toMatchObject({})
+  })
+  test('couple params', () => {
+    expect(parseQueryPath(['tag', '2', 'page', '1'])).toMatchObject({
+      page: 1,
+      tag: '2',
+    })
+  })
+  test('couple params with diff sequence', () => {
+    expect(parseQueryPath(['page', '1', 'tag', '2'])).toMatchObject({
+      page: 1,
+      tag: '2',
+    })
   })
 })
